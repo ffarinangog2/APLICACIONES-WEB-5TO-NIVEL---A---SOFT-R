01e83ecdab736aedc06ac4d12118210cf86dc33b
ivillamarinc <ivillamarinc@uteq.edu.ec>
2025-07-18 08:27:08 -0500

    Add files via upload

---
+++ SCLIC/src/main/java/com/uteqPWeb/SCLIC/SclicApplication.java
@@ -0,0 +1,13 @@
+package com.uteqPWeb.SCLIC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SclicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SclicApplication.class, args);
+	}
+
+}
---
+++ SCLIC/src/test/java/com/uteqPWeb/SCLIC/SclicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uteqPWeb.SCLIC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SclicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
